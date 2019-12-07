2d5621f38b6c2b6232345d9e74c28d977e841a72
LI0131 <mccannliam.lm@gmail.com>
2019-12-07 14:04:38 -0500

    Start vae framework

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ai-project4
+
+## Dev Setup
+1. Choose the project you would like to run (`cnn.py` or `cnn_cifar.py`) and execute: <br/>
+   `python3 cnn.py` <br/>
+   or <br/>
+   `python3 cnn_cifar.py`
+
+### 1D-CNN for IMDB dataset
+
+In this part of the project, we use a 1D-CNN to conduct sentiment analysis. We use a Dense layer of size 1 with the sigmoid activation function as our output layer in order to do this. The sigmoid function bounds the incoming signal to be between 0 and 1, so more positive sentiments will drive values which tend closer to 1, while more negative sentiments drive values which are closer to 0.
+
+We were able to achieve an accuracy of approx. 88% using a 1D-CNN model. We downloaded the dataset from the keras API which has a built in testing and training split of 50,000 training examples and 10,000 testing examples. We embed each input example using a keras tensorflow Embedding layer with the input dimension being the size of our vocabulary and the output dimension set to size (20,000, 64) -- that is each word is embedded as a size 64 vector. 
+
+
+### CNN for CIFAR-10 dataset
--- cnn_cifar.py
+++ cnn_cifar.py
@@ -1,40 +1,125 @@
 import os
 import keras
 import logging
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
 from keras.datasets import cifar10
-from keras.models import Sequential
+from keras.losses import binary_crossentropy
+from keras.models import Model, Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import Reshape, UpSampling2D, Input, Lambda
 
-logging.basicConfig(level=logging.INFO, filename='out.log')
+logging.basicConfig(level=logging.INFO, filename='info.log')
 
 
-BATCH_SIZE = os.environ.get('BATCH_SIZE', 32)
-NUM_CLASSES = os.environ.get('NUM_CLASSES', 10)
-EPOCHS = os.environ.get('EPOCHS', 50)
+BATCH_SIZE = int(os.environ.get('BATCH_SIZE', 32))
+NUM_CLASSES = int(os.environ.get('NUM_CLASSES', 10))
+EPOCHS = int(os.environ.get('EPOCHS', 50))
+
+
+def vae(x_train, y_train, x_test, y_test):
+
+    def sampling(args):
+        z_mean, z_log_var = args
+        batch = K.shape(z_mean)[0]
+        dim = K.int_shape(z_mean)[1]
+        epsilon = K.random_normal(shape=(batch, dim))
+        return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
+    image_size = x_train.shape[1]
+    original_dim = image_size * image_size
+    x_train = np.reshape(x_train, [-1, original_dim])
+    x_test = np.reshape(x_test, [-1, original_dim])
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+
+    # build encoder model
+    inputs = Input(shape=(original_dim, ), name='encoder_input')
+    x = Dense(512, activation='relu')(inputs)
+    z_mean = Dense(2, name='z_mean')(x)
+    z_log_var = Dense(2, name='z_log_var')(x)
+
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    z = Lambda(sampling, output_shape=(2,), name='z')([z_mean, z_log_var])
+
+    # instantiate encoder model
+    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+    encoder.summary()
+
+    # build decoder model
+    latent_inputs = Input(shape=(2,), name='z_sampling')
+    x = Dense(512, activation='relu')(latent_inputs)
+    outputs = Dense(original_dim, activation='sigmoid')(x)
+
+    # instantiate decoder model
+    decoder = Model(latent_inputs, outputs, name='decoder')
+    decoder.summary()
+
+    # instantiate VAE model
+    outputs = decoder(encoder(inputs)[2])
+    vae = Model(inputs, outputs, name='vae_mlp')
+
+    reconstruction_loss = binary_crossentropy(inputs, outputs)
+    reconstruction_loss *= original_dim
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+    vae.add_loss(vae_loss)
+
+    vae.compile(optimizer='adam')
+
+    vae.fit(
+        x_train,
+        epochs=1,
+        batch_size=128,
+        shuffle=True,
+        validation_data=(x_test, None),
+    )
+
+    x_exp = []
+    y_exp = []
+    for i in range(len(x_train)):
+        x_exp.append(vae.predict(
+            tf.reshape(x_train[i], [1024,])
+        ))
+        y_exp.append(y_train[i])
+
+    return x_train.extend(x_exp), y_train.extend(y_exp)
 
 
 def run():
+
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-    y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
+    # use a variational autoencoder to increase training set size
+    x_train, y_train = vae(x_train, y_train, x_test, y_test)
+
+    # convert the image pixel values to a range between 0 and NUM_CLASSES for categorical classification
+    y_train = keras.utils.to_categorical(y_train, NUM_CLASSES) 
     y_test = keras.utils.to_categorical(y_test, NUM_CLASSES)
 
+    # increase the number of filters over time to deal with increasingly complex features
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), padding='same',
-                    input_shape=x_train.shape[1:]))
+    # same padding pads the input so the output dimensions will be identical
+    # using a window size of 3 x 3 with 32 filters
+    model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Conv2D(32, (3, 3)))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
     model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
     model.add(Flatten())
     model.add(Dense(512))
@@ -43,16 +128,13 @@ def run():
     model.add(Dense(NUM_CLASSES))
     model.add(Activation('softmax'))
 
+    logging.info(model.summary())
+
     model.compile(
         loss='categorical_crossentropy',
         optimizer='adam',
         metrics=['accuracy']
     )
-
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
 
     model.fit(
         x_train, y_train,
